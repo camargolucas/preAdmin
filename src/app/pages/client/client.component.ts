3099047fe57979bafb1edd90a1b0bf1e033afdbc
thiagoamaci <46783586+thiagoamaci@users.noreply.github.com>
2019-03-19 11:51:53 -0300

    Paginação

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -12,10 +12,34 @@ import {Router} from '@angular/router';
 })
 export class ClientComponent implements OnInit {
 
+  //#######################################################
+  //Usado para receber o status de login do usuário
   userLogged:boolean = false;
+  //#######################################################
+
+  //#######################################################
+  //Array que vai conter os resultados da busca do usuário
   arrClientList:any;
+  //#######################################################
+
+  //#######################################################
+  //Array que contém todos os registros da base, isso é usado para compor uma nova busca
+  arrClientListAll:any;
+  //#######################################################
+  //#######################################################
+  //Variavel que recebe os erros reportados
   error:any;
+  //#######################################################
+
+  //#######################################################
+  //Variavel que recebe a busca realizada pelo usuário
   searchName;
+  //#######################################################
+
+  //#######################################################
+  //Variavel que armazena o total de resultados encontrados
+  totalItensBusca:number;
+  //#######################################################
   // pager object
   pager: any = {};
   // paged items
@@ -28,29 +52,68 @@ export class ClientComponent implements OnInit {
     private storageService:StorageService,
     private paginationService:PagerService
     ) { 
+    //#######################################################
+    //Verificamos de o usuário esta logado, true para logado e false para não logado
     this.userLogged = loginService.checkIfUserIsLogged();
+    //#######################################################
+
+    //#######################################################
+    //Busca a lista de clientes na API, ao receber os dados, 
+    //atualiza a lista de dados no cache
     this.getAllClientData();
+    //#######################################################
+
+    //#######################################################
+    //Resgata a lista de clientes no cache do usuário
+    //esta variável irá conter os dados parciais dependendo
+    //da busca do usuário por resultados
     this.arrClientList = storageService.getAllClientList();
+    //#######################################################
+
+    //#######################################################
+    //Busca todos os dados da lista de clientes do cache, isso
+    //serve como base para a busca subsequente pelo usuário
+    this.arrClientListAll = storageService.getAllClientList();
+    //#######################################################
+
+    //#######################################################
+    //define a página atual para o serviço de paginação
     this.setPage(1);
+    //#######################################################
+
+    //#######################################################
+    //variável que recebe o total de itens do resultado de busca
+    //necessário para controlar e exibir mensagem de resultado
+    //não encontrado
+    try{
+      this.totalItensBusca = this.arrClientList.length;
+    }catch(e){
+    }
+    
+    //#######################################################
   }
 
   ngOnInit() {
+    //##############################################
+    //Se o usuário não estiver logado, o 
+    //sistema redireciona para a página de login
     if(!this.userLogged){
       this.router.navigateByUrl('/login');
     }
+     //##############################################
   }
-
 
   public getAllClientData(){
 
     try{
       this.clientService.getClientList().subscribe(data => {
-
-        //console.log("this.arrClientList.length : " + this.arrClientList);
-        if(this.arrClientList ==null){
+        //###########################################################
+          this.arrClientListAll = data;
+         
+          if(this.arrClientList == null){
             this.arrClientList = data;
-        }
-        
+          }
+        //###########################################################
           this.storageService.insertCacheClientList(data);
           this.setPage(1);
         }, error => {
@@ -64,17 +127,28 @@ export class ClientComponent implements OnInit {
 
   }
 
+  filterData(){
+    this.arrClientList = this.arrClientListAll.filter(data => {
+      return data.NOME
+        .toLowerCase()
+        .startsWith(this.searchName.toLowerCase());
+    });
+    this.totalItensBusca = this.arrClientList.length;
+    this.setPage(1);
+  }
+
   setPage(page: number) {
     try{
     // get pager object from service
     this.pager = this.paginationService.getPager(this.arrClientList.length, page);
     // get current page of items
     this.pagedItems = this.arrClientList.slice(this.pager.startIndex, this.pager.endIndex + 1);
- 
+
     }catch(e){
       console.log(e);
     }
-
   }
+
+
 
 }
--- src/app/pages/economic-group/economic-group.component.ts
+++ src/app/pages/economic-group/economic-group.component.ts
@@ -2,7 +2,9 @@ import { pipe } from '@angular/core/src/render3';
 import { Component, OnInit } from '@angular/core';
 import { EconomicGroupService } from 'src/app/services/economic-group.service';
 import { StorageService } from "./../../services/storage.service";
-
+import { PagerService } from './../../services/pager.service';
+import { LoginService } from '../../services/login.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-economic-group',
   templateUrl: './economic-group.component.html',
@@ -10,28 +12,114 @@ import { StorageService } from "./../../services/storage.service";
 })
 export class EconomicGroupComponent implements OnInit {
 
+  //#######################################################
+  //Usado para receber o status de login do usuário
+  userLogged:boolean = false;
+  //#######################################################
+
   arrEconominGroup;
-  error;
+
+  arrEconominGroupAll;
+
+   //#######################################################
+  //Variavel que recebe os erros reportados
+  error:any;
+  //#######################################################
+
+  //#######################################################
+  //Variavel que recebe a busca realizada pelo usuário
+  searchName;
+  //#######################################################
+
+  //#######################################################
+  //Variavel que armazena o total de resultados encontrados
+  totalItensBusca:number;
+  //#######################################################
+  // pager object
+  pager: any = {};
+  // paged items
+  pagedItems: any[];
+
+
   constructor(
+    private router: Router,
+    private loginService:LoginService,
     public service:EconomicGroupService,
-    private storage: StorageService
-    ) {}
+    private storage: StorageService,
+    private paginationService:PagerService
+    ) {
+      this.arrEconominGroup = this.storage.getAllDataEconomicGroup();
+      this.arrEconominGroupAll = this.storage.getAllDataEconomicGroup();
+      this.getAllEconomicGroupData();
+    //#######################################################
+    //define a página atual para o serviço de paginação
+    this.setPage(1);
+    //#######################################################
+    //#######################################################
+    //Verificamos de o usuário esta logado, true para logado e false para não logado
+    this.userLogged = loginService.checkIfUserIsLogged();
+    //#######################################################
+
+    //#######################################################
+    //variável que recebe o total de itens do resultado de busca
+    //necessário para controlar e exibir mensagem de resultado
+    //não encontrado
+    try{
+      this.totalItensBusca = this.arrEconominGroup.length;
+    }catch(e){
+    }
+    
+    //#######################################################
+    }
 
   ngOnInit() {
-    this.arrEconominGroup = this.storage.getAllDataEconomicGroup();
-    this.getAllEconomicGroupData();
+    //##############################################
+    //Se o usuário não estiver logado, o 
+    //sistema redireciona para a página de login
+    if(!this.userLogged){
+      this.router.navigateByUrl('/login');
+    }
+     //##############################################
   }
 
   public getAllEconomicGroupData(){
     try{
       this.service.getEconomicGroupList().subscribe(data => {
-          this.arrEconominGroup = data;
+        
+          this.arrEconominGroupAll = data;
+
+          if(this.arrEconominGroup == null){
+            this.arrEconominGroup = data;
+          }
+          
           this.storage.insertCacheEconomicGroup(data);
         }, error => {
           this.error = error
           console.log("ERRO AO BUSCAR DADOS");
         }
       );
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  filterData(){
+    this.arrEconominGroup = this.arrEconominGroupAll.filter(data => {
+      return data.nomeGrupoEconomicoCliente
+        .toLowerCase()
+        .startsWith(this.searchName.toLowerCase());
+    });
+    this.totalItensBusca = this.arrEconominGroup.length;
+    this.setPage(1);
+  }
+
+  setPage(page: number) {
+    try{
+    // get pager object from service
+    this.pager = this.paginationService.getPager(this.arrEconominGroup.length, page);
+    // get current page of items
+    this.pagedItems = this.arrEconominGroup.slice(this.pager.startIndex, this.pager.endIndex + 1);
+
     }catch(e){
       console.log(e);
     }
