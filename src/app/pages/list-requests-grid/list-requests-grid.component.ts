1b053a42e288e7e1160f12c7e17597b151077b85
thiagoamaci <46783586+thiagoamaci@users.noreply.github.com>
2019-03-29 12:49:04 -0300

    Array Lista

--- src/app/pages/list-requests-grid/list-requests-grid.component.ts
+++ src/app/pages/list-requests-grid/list-requests-grid.component.ts
@@ -12,15 +12,101 @@ export class ListRequestsGridComponent implements OnInit {
   screenHeight:number;
   screenWidth:number;
 
+  //Status dos Pprodutos
+  //1.Em evidencia
+  //2.Em falta
+  //3.Entre Safra
+  //4.Neutro
+  //Em evidencia, Em falta, Entre Safra, Neutro
+
   dataResponsiveGridBodyHeight:number = 100;
   selectedRow = "row_";
 
+  arrOrder = "DESC";
+  tabarrOrderSelected = "";
 
-  dataArrGrid = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
-  dataArrGridColum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
+  dataArrGrid = [
+      {
+        "id":1,
+        "nome":"A abacate 1",
+        "status":1,
+        "tipo":"F",
+        "unidmedida":"Unidade",
+        "peso":"18,00"
+      },{
+        "id":2,
+        "nome":"B abacate 2",
+        "status":2,
+        "tipo":"F",
+        "unidmedida":"Kilo",
+        "peso":"16,00"
+      },{
+        "id":3,
+        "nome":"C abacate 3",
+        "status":3,
+        "tipo":"F",
+        "unidmedida":"Bandeja",
+        "peso":"15,00"
+      },
+      {
+        "id":4,
+        "nome":"D abacate 4",
+        "status":4,
+        "tipo":"F",
+        "unidmedida":"Unidade",
+        "peso":"14,00"
+      },
+      {
+        "id":5,
+        "nome":"X abacate 5",
+        "status":1,
+        "tipo":"F",
+        "unidmedida":"Unidade",
+        "peso":"13,00"
+      },
+      {
+        "id":6,
+        "nome":"F abacate 6",
+        "status":2,
+        "tipo":"F",
+        "unidmedida":"Unidade",
+        "peso":"28,00"
+      },
+      {
+        "id":7,
+        "nome":"G abacate 7",
+        "status":3,
+        "tipo":"F",
+        "unidmedida":"Unidade",
+        "peso":"100,00"
+      },
+      {
+        "id":8,
+        "nome":"H abacate 8",
+        "status":4,
+        "tipo":"F",
+        "unidmedida":"Unidade",
+        "peso":"7,00"
+      },
+      {
+        "id":9,
+        "nome":"I abacate 9",
+        "status":1,
+        "tipo":"F",
+        "unidmedida":"Unidade",
+        "peso":"54,00"
+      },
+      {
+        "id":10,
+        "nome":"J abacate 10",
+        "status":2,
+        "tipo":"F",
+        "unidmedida":"Unidade",
+        "peso":"167,00"
+      }
+  ];
+  dataArrGridColum = [1,2,3,4,5,6,7,8,9,10];
   //####################################################
-
-
 
   //####################################################
     //####################################################
@@ -40,16 +126,12 @@ export class ListRequestsGridComponent implements OnInit {
 
   ngOnInit() {
     this.getHeightWidthOnStart();
-
-
-
     this.totalVolumes = 850;
     this.valorTotalVendas = 1500;
     this.valorTotalVendasComDesconto = 2800;
     this.valorTotalCompra = 3.500;
     this.resultado = 4000;
     this.margem = '3%';
-
   }
 
   onResize(event) {
@@ -60,8 +142,6 @@ export class ListRequestsGridComponent implements OnInit {
     //######################################################
   }
 
-
-
   @HostListener('window:resize', ['$event'])
     getHeightWidthOnStart(event?) {
       this.screenHeight = window.innerHeight;
@@ -70,16 +150,47 @@ export class ListRequestsGridComponent implements OnInit {
       this.dataResponsiveGridBodyHeight = this.screenHeight - (91+167);
     }
 
-
     @HostListener('window:scroll', ['$event']) // for window scroll events
       onScroll(event) {
         document.getElementById('dataResponsiveGridShowRight').scrollLeft = event.srcElement.scrollLeft;
         document.getElementById('gridTableDataLeftContainer').scrollTop = event.srcElement.scrollTop;
         
      }
-
     highlightRow(row:any){
       this.selectedRow = row;
     }
+    orderDataArrGrid(attribute:any){
+
+      this.setTabarrOrderSelected(attribute);
+      
+      if(this.arrOrder == 'ASC'){
+        this.dataArrGrid = this.sortByKeyASC(this.dataArrGrid, attribute);
+        this.arrOrder = 'DESC';
+      }else{
+        this.dataArrGrid = this.sortByKeyDESC(this.dataArrGrid, attribute);
+        this.arrOrder = 'ASC';
+      }
+
+    }
+
+  sortByKeyASC(array, key) {
+      return array.sort(function(a, b) {
+          var x = a[key]; 
+          var y = b[key];
+          return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+      });
+  }
+
+  sortByKeyDESC(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; 
+        var y = b[key];
+        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+    });
+  }
+  setTabarrOrderSelected(tab:any){
+    this.tabarrOrderSelected = tab;
+  }
+
 
 }
